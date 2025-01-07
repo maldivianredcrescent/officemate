9ac106b6d4c20fccb972577dfa7275fa1a175b0f
Maldivian Red Crescent <mrchub@redcrescent.org.mv>
2025-01-07 17:22:09 +0500

    some more updates

--- src/actions/dashboardActions.js
+++ src/actions/dashboardActions.js
@@ -13,31 +13,70 @@ export const getDashboardRequestsAction = actionClient
     })
   )
   .action(async ({ parsedInput }) => {
-    const requests = await prisma.request.findMany({
-      where: parsedInput.type ? { type: parsedInput.type } : undefined,
-      orderBy: { updatedAt: "desc" },
-      include: {
-        unit: true,
-        activity: {
-          include: {
-            workplan: true,
-            project: {
-              include: {
-                donor: true,
+    if (
+      [
+        "admin",
+        "budget_approver",
+        "finance_approver",
+        "payment_processor",
+      ].includes(user.role)
+    ) {
+      const requests = await prisma.request.findMany({
+        where: parsedInput.type ? { type: parsedInput.type } : undefined,
+        orderBy: { updatedAt: "desc" },
+        include: {
+          unit: true,
+          activity: {
+            include: {
+              workplan: true,
+              project: {
+                include: {
+                  donor: true,
+                },
               },
             },
           },
         },
-      },
-      skip: parsedInput.skip,
-      take: parsedInput.limit,
-    });
+        skip: parsedInput.skip,
+        take: parsedInput.limit,
+      });
 
-    const totalRequests = await prisma.request.count({
-      where: parsedInput.type ? { type: parsedInput.type } : undefined,
-    });
-    const activities = await prisma.activity.findMany({
-      include: { workplan: true },
-    });
-    return { requests, totalRequests, activities, success: true };
+      const totalRequests = await prisma.request.count({
+        where: parsedInput.type ? { type: parsedInput.type } : undefined,
+      });
+      const activities = await prisma.activity.findMany({
+        include: { workplan: true },
+      });
+      return { requests, totalRequests, activities, success: true };
+    } else {
+      const requests = await prisma.request.findMany({
+        where: parsedInput.type
+          ? { type: parsedInput.type, unitId: user.unitId }
+          : { unitId: user.unitId },
+        orderBy: { updatedAt: "desc" },
+        include: {
+          unit: true,
+          activity: {
+            include: {
+              workplan: true,
+              project: {
+                include: {
+                  donor: true,
+                },
+              },
+            },
+          },
+        },
+        skip: parsedInput.skip,
+        take: parsedInput.limit,
+      });
+
+      const totalRequests = await prisma.request.count({
+        where: parsedInput.type ? { type: parsedInput.type } : undefined,
+      });
+      const activities = await prisma.activity.findMany({
+        include: { workplan: true },
+      });
+      return { requests, totalRequests, activities, success: true };
+    }
   });
